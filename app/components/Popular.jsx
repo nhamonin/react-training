9085c6a4ebf96c683074bbf207b2a51d202ab918
nhamonin <nazarii.hamonin@gmail.com>
2023-05-29 11:54:37 +0300

    Add language selection functionality to Popular component

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,15 +1,34 @@
 import * as React from 'react';
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All',
+    };
+
+    this.updateLanguage = (language) => {
+      this.setState({
+        selectedLanguage: language,
+      });
+    };
+  }
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
     return (
-      <select>
-        {languages.map((language) => (
-          <option key={language}>{language}</option>
-        ))}
-      </select>
+      <main>
+        <select
+          onChange={(e) => this.updateLanguage(e.target.value)}
+          selected={this.state.selectedLanguage}
+        >
+          {languages.map((language) => (
+            <option key={language}>{language}</option>
+          ))}
+        </select>
+        {JSON.stringify(this.state, null, 2)}
+      </main>
     );
   }
 }
