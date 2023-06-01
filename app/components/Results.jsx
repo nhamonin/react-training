b856c2328009619d8e78efb471db2cb2518ce745
nhamonin <nazarii.hamonin@gmail.com>
2023-06-01 11:39:34 +0300

    Add functionality to fetch and calculate player scores in Results component

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 
+import { battle } from '../utils/api';
+
 export default class Battle extends React.Component {
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props;
+
+    battle([playerOne, playerTwo]).then((players) => {
+      console.log('data: ', players);
+    });
+  }
   render() {
-    return (
-      <div>
-        Results
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
-      </div>
-    );
+    return <div>Results</div>;
   }
 }
--- app/utils/api.js
+++ app/utils/api.js
@@ -13,3 +13,65 @@ export function fetchPopularRepos(language) {
       return data.items;
     });
 }
+
+function getErrorMsg(message, username) {
+  if (message === 'Not Found') {
+    return `${username} doesn't exist`;
+  }
+
+  return message;
+}
+
+async function getProfile(username) {
+  return fetch(`https://api.github.com/users/${username}`)
+    .then((res) => res.json())
+    .then((profile) => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username));
+      }
+
+      return profile;
+    });
+}
+
+async function getRepos(username) {
+  return fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
+    .then((res) => res.json())
+    .then((repos) => {
+      if (repos.message) {
+        throw new Error(getErrorMsg(profile.message, username));
+      }
+
+      return repos;
+    });
+}
+
+async function getUserData(player) {
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
+      profile,
+      score: calculateScore(profile.followers, repos),
+    })
+  );
+}
+
+function calculateScore(followers, repos) {
+  return followers * 3 + getStarCount(repos);
+}
+
+function getStarCount(repos) {
+  return repos.reduce(
+    (count, { stargazers_count }) => count + stargazers_count,
+    0
+  );
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+export async function battle(players) {
+  return Promise.all([getUserData(players[0]), getUserData(players[1])]).then(
+    sortPlayers
+  );
+}
