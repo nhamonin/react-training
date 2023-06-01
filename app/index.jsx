5d081c4ea2ec558ac8a93e273bfaf663de789c4a
nhamonin <nazarii.hamonin@gmail.com>
2023-06-01 20:00:15 +0300

    Add Nav component and theme toggling functionality

--- app/index.jsx
+++ app/index.jsx
@@ -3,15 +3,31 @@ import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './index.css';
+import Nav from './components/Nav';
 import Popular from './components/Popular';
 import Battle from './components/Battle';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'dark',
+    };
+  }
+
+  toggleTheme = () => {
+    this.setState(({ theme }) => ({
+      theme: theme === 'light' ? 'dark' : 'light',
+    }));
+  };
+
   render() {
     return (
       <Router>
-        <div className="light">
+        <div className={this.state.theme}>
           <div className="container">
+            <Nav theme={this.state.theme} toggleTheme={this.toggleTheme} />
             <Routes>
               <Route path="/" element={<Popular />} />
               <Route path="/battle" element={<Battle />} />
